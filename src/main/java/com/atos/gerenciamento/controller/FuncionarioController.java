9bdc600a768b244a7c0a6df3d092c950cf72d060
ferrarifa <ferrarifabio600@gmail.com>
2026-04-14 17:29:38 -0300

    inicializacao

---
+++ src/main/java/com/atos/gerenciamento/controller/FuncionarioController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.atos.gerenciamento.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Aluno
+ */
+@Controller
+public class FuncionarioController {
+    
+    @GetMapping("/funcionarios")
+    public String getFuncionarios(){
+        return "funcionarios";
+    }
+    
+}
---
+++ src/main/java/com/atos/gerenciamento/controller/service/FuncionarioService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.atos.gerenciamento.controller.service;
+
+import com.atos.gerenciamento.controller.model.FuncionarioBean;
+import com.atos.gerenciamento.controller.repository.FuncionarioDAO;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Aluno
+ */
+@Service
+public class FuncionarioService {
+    
+    @Autowired
+    private FuncionarioDAO repository;
+    
+    public List<FuncionarioBean> lerTodos (){
+    return repository.letTodos();
+}
+    
+}
